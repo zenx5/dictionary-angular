e3c756a865739097a0ca8b58f904b48a658a59e5
Octavio Martinez <omartinez1618@gmail.com>
2023-08-15 12:21:25 -0400

    Updates

---
+++ assets/type.ts
@@ -0,0 +1,51 @@
+export interface DictionaryResult {
+    word:       string;
+    phonetics:  Phonetic[];
+    phonetic:   Phonetic;
+    meanings:   Meaning[];
+    license:    License;
+    sourceUrls: string[];
+}
+
+export interface License {
+    name: string;
+    url:  string;
+}
+
+export interface Meaning {
+    partOfSpeech: string;
+    definitions:  Definition[];
+    synonyms:     string[];
+    antonyms:     string[];
+}
+
+export interface Definition {
+    definition: string;
+    synonyms:   any[];
+    antonyms:   any[];
+    example?:   string;
+}
+
+export interface Phonetic {
+    audio:      string;
+    sourceUrl?: string;
+    license?:   License;
+    text?:      string;
+}
+
+export interface SettingOption {
+    name:       string;
+    label:      string;
+    value:      boolean;
+}
+
+export interface ResponseError {
+    title:      string;
+    message:    string;
+    resolution: string;
+}
+
+export interface ResponseQuery {
+    error:      ResponseError|null;
+    success:    Array<DictionaryResult>;
+}
